576aa3dc29472c232e61545318217c96ea0ae589
Mike <migueldiganchi@gmail.com>
2023-09-09 05:10:17 -0300

    Removed unused line

--- pages/game.js
+++ pages/game.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import GameBoard from "../components/Game/GameBoard";
 import Page from "../components/Page";
-import Link from "next/link";
 
 const GamePage = () => {
   const [isGaming, setIsGaming] = useState(false);
